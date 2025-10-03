598ecfc2ca673f1162a6b63174d9d1e6f5ec3981
getwithJ4 <joshua.goods.student@madisonpsb.org>
2025-10-03 14:41:41 +0000

    finalized plateformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,23 +27,30 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
-    // TODO 3 - Create Collectables
-
-
-
+createPlatform(500,700,500,800);
+createPlatform(600,600,600,700,);
+createPlatform(800,600,800,800,);
+createPlatform(700,500,800,800,"hotpink");
+createPlatform(400,800,800,900,);
+createPlatform(1350, 400, 50, 50, "red");
+createPlatform(1150, 300, 50, 50, "yellow");
+createPlatform(1000, 200, 50, 50, "green");
+createPlatform(600, 100, 300, 50, "blue");
+    // TODO 3 - Create Collectables;
+createCollectable("diamond", 700, 90, 0.5, 0.7);
+createCollectable("steve",1350, 300,);
+createCollectable("diamond", 400, 90, 0.5, 0.7);
+createPlatform(400, 300, 300, 50, "blue");
+createPlatform(800, 400, 50, 50, "red");
     
     // TODO 4 - Create Cannons
-
-
-    
+createCannon("right", 300, 2000);
+createCannon("left", 300, 2000);
+createCannon("left", 500, 2000);
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
